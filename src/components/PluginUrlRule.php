82041c097e1753b2cf3a0caf5e45b48ec8c5bd3f
zhangyonghui <zhangyonghui@zhangyonghuideMacBook-Pro.local>
2019-12-01 16:38:05 +0800

    修改框架文件

---
+++ src/components/PluginUrlRule.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace zyh\plugins\components;
+
+
+use yii\base\BaseObject;
+use yii\web\UrlRuleInterface;
+
+class PluginUrlRule extends BaseObject implements UrlRuleInterface
+{
+
+
+    public function parseRequest($manager, $request)
+    {
+
+        if (!$request->pathInfo) {
+            return false;
+        }
+
+        $pathArr = explode('/', $request->pathInfo);
+
+        // 插件广场路由规则
+        if (in_array($request->pathInfo, ['plugins/index', 'plugins'])) {
+            return ['plugins/plugins/index', []];
+        }
+
+        // 插件路由规则
+        $plugins = Common::getCache($pathArr[0], 'plugins', []);
+        if ($plugins) {
+            $num = count($pathArr);
+            $url = $num == 1 ? '' : $request->pathInfo;
+
+            switch ($num) {
+                case 1:
+                    $url = $pathArr[0] . '/index';
+                case 2:
+                    $url = $pathArr[0] . '/' . $url;
+            }
+            return ['plugins/' . $url, []];
+        }
+
+        return false; // this rule does not apply
+    }
+
+    public function createUrl($manager, $route, $params)
+    {
+
+        return false;
+    }
+}
---
+++ src/example/menu/views/test/index.php
@@ -0,0 +1,9 @@
+<?php
+
+
+
+?>
+show views test index.php
+
+
+<?=\Yii::t('menu/test','test');?>
